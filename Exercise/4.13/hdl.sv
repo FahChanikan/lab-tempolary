71ca43b6f29e5132dc1fb5a53a99e6a9a18786d1
chanikan <chanikan.n91@gmail.com>
2025-12-31 00:09:11 +0800

    4.13

---
+++ Exercise/4.13/hdl.sv
@@ -0,0 +1,16 @@
+module decorder_2to4(
+    input logic [1:0] a,
+    output logic [3:0] y
+);
+
+always_comb begin
+    case(a)
+        2'b00: y = 4'b0001; //when a=00, bit 0
+        2'b01: y = 4'b0010;
+        2'b10: y = 4'b0100;
+        2'b11: y = 4'b1000;
+    endcase
+end
+
+endmodule
+
---
+++ Exercise/4.13/testbench.sv
@@ -0,0 +1,20 @@
+module decorder_2to4_tb;
+
+    logic [1:0] a;
+    logic [3:0] y;
+
+    decorder_2to4 dut(
+        .a(a),
+        .y(y)
+    );
+
+    initial begin
+        a = 2'b00; #10;
+        a = 2'b01; #10;
+        a = 2'b10; #10;
+        a = 2'b11; #10;
+
+        $finish;
+    
+    end
+endmodule
